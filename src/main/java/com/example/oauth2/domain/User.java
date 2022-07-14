034c3e56fd2e4539f50a65670882737a08b3322e
상민 <sangmin@sangmin-ui-MacBookPro.local>
2022-07-15 08:47:42 +0900

    Initial commit

---
+++ src/main/java/com/example/oauth2/domain/User.java
@@ -0,0 +1,22 @@
+package com.example.oauth2.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+
+}
